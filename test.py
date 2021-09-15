24ef062f82f2885e8ca4ed6f67a75499dcfffc5c
DaHunterTime <ignacio.huntley@uc.cl>
2021-09-14 22:28:15 -0300

    Renamed tests.py -> test.py to avoid name conflict

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from tests.thread_test_1 import main as thread_1
 
 
 if __name__ == "__main__":
-    all_funcs = {"thread 1": thread_1}
+    all_funcs = {"thread_1": thread_1}
     text = ("Type the name of the test you want to run\n"
             "-----------------------------------------\n"
             "Options:\n"
